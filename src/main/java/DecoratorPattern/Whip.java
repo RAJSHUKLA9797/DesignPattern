6156485494f083f267886575e680a3a259eb9d91
Raj Shukla <rajshukla9797@gmail.com>
2026-02-07 23:23:49 +0530

    added size option in beverage

--- src/main/java/DecoratorPattern/Whip.java
+++ src/main/java/DecoratorPattern/Whip.java
@@ -7,11 +7,25 @@ public class Whip extends Decorator{
     }
     @Override
     public String getDescription() {
-        return (this.beverage.getDescription()+ ", Whip");
+        return (this.beverage.getDescription()+ " " + this.beverage.getSize() + ", Whip");
     }
 
     @Override
     public double getCost() {
-        return this.beverage.getCost()+0.75;
+//        return( this.beverage.getCost()+0.99);
+        switch (this.beverage.getSize()){
+            case TALL -> {
+                return this.beverage.getCost() +1.99;
+            }
+            case GRANDE -> {
+                return this.beverage.getCost()+2.5;
+            }
+            case VENTI -> {
+                return this.beverage.getCost()+3.5;
+            }
+            default -> {
+                return this.beverage.getCost();
+            }
+        }
     }
 }
